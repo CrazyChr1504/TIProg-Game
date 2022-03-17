0a987d301b1e3a2e7be455630a5108953f507c68
CrazyChr1504 <chris95504@gmail.com>
2022-03-17 13:35:03 +0100

    Completed code, with eggs

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # Imports
 from resourses import Character, Monster, save_character, load_characters
+from time import sleep
 import random
 
 # Global variables
@@ -32,11 +33,13 @@ def new_fight(players: list, enemies: list):
             else:
                 players.remove(target)
             participants.remove(target)
+            sleep(1)
         
         else:
             print(f"{char.get_name()} was attack by {target.get_name()}.")
             print(f"{char.get_name()} has {char.get_health()} hp left.")
-        
+            sleep(1)
+
         if len(players) == 0 or len(enemies) == 0:
             break
         turn += 1
@@ -148,44 +151,90 @@ def main():
     if choice.lower() == "y":
         stat_bonus = character_choice()
         stat = stat_choice(stat_bonus)
-        hps = stat[0]
-        atks = (stat[1]//2)
-        defs = (stat[2]//4)
-        p1 = Character(cn, hps, atks, defs)
-        players.append(p1)
+        
+        if cn != "Adam":
+            hps = (stat[0] + 10)
+            atks = ((stat[1]//2) + 3)
+            defs = ((stat[2]//4) + 2)
+            p1 = Character(cn, hps, atks, defs)
+            players.append(p1)
+        
+        elif cn == "Max":
+            hps = (stat[0] + 5)
+            atks = ((stat[1]//2) + 2)
+            defs = ((stat[2]//4) + 3)
+            p1 = Character(cn, hps, atks, defs)
+            players.append(p1)
 
-    if cn == "Adam":
-        battle = input(f"\nWould you like to proceed to the battlefield, Scrawny {cn}? (y/n)\nAnswer: ")
-        if battle.lower() == "n":
-            print("Sorry the monsters have started their advance. GLHF!\n")
-    
+        elif cn == "Aqua":
+            hps = (stat[0] + 15)
+            atks = ((stat[1]//2) + 3)
+            defs = ((stat[2]//4) + 4)
+            p1 = Character(cn, hps, atks, defs)
+            players.append(p1)
+
+        else:
+            hps = 5
+            atks = 1
+            defs = 0
+            p1 = Character(cn, hps, atks, defs)
+            players.append(p1)
+
+    if cn != "":
+        if cn == "Adam":
+            battle = input(f"\nWould you like to proceed to the battlefield, Scrawny {cn}? (y/n)\nAnswer: ")
+
+        elif cn == "Aqua":
+            battle = input(f"\nWould you like to proceed to the battlefield, Shmort {cn}? (y/n)\nAnswer: ")
+            if battle.lower() == "n":
+                print("Sorry the monsters have started their advance to isekai you. GLHF!\n")
+        
+        elif cn == "Max":
+            battle = input(f"\nWould you like to proceed to the battlefield, Skinny Stick Boi? (y/n)\nAnswer: ")
+        
+        if cn != "Aqua" and cn == "n":
+            print("Sorry the monsters have started their advance. GLHF.\n")
+        
     random.shuffle(monster_name)
     for _ in range(random.randint(2,4)):
-        enemies.append(Monster(monster_name[mn], random.randint(14,34), random.randint(1,5), random.randint(2,5)))
-        mn += 1
+        
+        if monster_name[mn] == "Battlebubbles":
+            enemies.append(Monster(monster_name[mn], 420, random.randint(5,9), random.randint(4,7)))
+            mn += 1
+
+        else:
+            enemies.append(Monster(monster_name[mn], random.randint(15,69), random.randint(2,6), random.randint(1,5)))
+            mn += 1
+    
     
     print("Your characters:")
     for _ in range(len(players)):
         print(f"{players[_]}\n")
+    save_character(players)
     
-    print(f"A horde of monsters has apeared.")
-    for _ in range(len(enemies)):
-        print(f"{enemies[_]}\n")
-
+    if len(enemies) == 2:
+        print(f"A pair of monsters has apeared.")
+        for _ in range(len(enemies)):
+            print(f"{enemies[_]}\n")
+    
+    else:
+        print(f"A horde of monsters has apeared.")
+        for _ in range(len(enemies)):
+            print(f"{enemies[_]}\n")
     
     while len(enemies) != 0 and len(players) != 0:
         new_fight(players, enemies)
 
     if len(enemies) == 0:
-        print("The players won!")
-        print("Would you like to save your character? (y/n)")
+        print("\nThe players won!\nGG!")
+        print("\nWould you like to save your character? (y/n)")
         save = input("Enter here: ")
         if save.lower() ==  "y":
             save_character(players)
 
     elif len(players) == 0:
-        print("The Monsters won!")
+        print("\nThe Monsters won!")
         print("GAME OVER")
-    
+
 if __name__ == "__main__":
     main()
